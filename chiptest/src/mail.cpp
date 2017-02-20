15ee162d9e4bf4330e0e1be1241c6ed0239e9b54
Oleg Usanov <oleg@usanov.net>
2017-02-20 18:47:51 +0200

    z

--- chiptest/src/mail.cpp
+++ chiptest/src/mail.cpp
@@ -1,28 +1,14 @@
-/**
- * Blink
- *
- * Turns on an LED on for one second,
- * then off for one second, repeatedly.
- */
-#include "Arduino.h"
+// для SoftSerial скорость порта в 115200 является большой и не гарантирует стабильную работу
+const int COM_BAUD = 115200;
+ 
+void setup() {
 
-void setup()
-{
-  // initialize LED digital pin as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(COM_BAUD);
+  Serial.println("Echo setup done");
+}
+ 
+void loop() {
+  if ( Serial.available() )
+    Serial.write( Serial.read() );
 }
 
-void loop()
-{
-  // turn the LED on (HIGH is the voltage level)
-  digitalWrite(LED_BUILTIN, HIGH);
-
-  // wait for a second
-  delay(1000);
-
-  // turn the LED off by making the voltage LOW
-  digitalWrite(LED_BUILTIN, LOW);
-
-   // wait for a second
-  delay(1000);
-}
